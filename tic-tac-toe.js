8bb1fdb2a5010ead7c02af6b6365bac9a03da770
Franco Portin <fportin350@gmail.com>
2021-01-07 14:16:23 -0800

    Working on requirement 2

---
+++ tic-tac-toe.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const board = document.getElementById('tic-tac-toe-board');
+    const squares = board.children;
+
+    board.addEventListener('click', event => {
+
+
+        event.target.style.value
+        console.log([0]);
+
+    })
+
+
+
+
+
+})
