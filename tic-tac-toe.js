4634e4ebcac51f78b4a6ed78f214ef1f7be71d75
Franco Portin <fportin350@gmail.com>
2021-01-07 15:26:23 -0800

    Added features

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,12 +2,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const board = document.getElementById('tic-tac-toe-board');
     const squares = board.children;
+    let counter = 0;
 
     board.addEventListener('click', event => {
         console.log(event.target);
-        if (event.target.innerText === '') {
-            event.target.innerHTML = ''
-        }
+        event.target.innerHTML = '<img src = "ticTacImg/player-x.svg"></img>';
+        event.target.innerHTML = '<img src = "ticTacImg/player-o.svg"></img>'
+        // if (event.target.innerText === '') {
+        //     event.target.innerHTML = ''
+        // }
     });
 
 
