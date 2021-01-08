3da15c21337d7b9f2cd8ff4dfbb2eb846cf98481
Franco Portin <fportin350@gmail.com>
2021-01-08 12:37:25 -0800

    Update on game but with bugs

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,13 +3,15 @@ window.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('tic-tac-toe-board');
     const squares = board.children;
     let counter = 0;
-    let checkArray = ['', '', '', '', '', '','', '', ''];
+    let checkArray = ['', '', '', 
+                       '', '', '', 
+                       '', '', ''];
 
     board.addEventListener('click', event => {
         let target = event.target;
 
         let squareNum = Number.parseInt(target.id[target.id.length - 1]);
-        console.log(event.target);
+        // console.log(squareNum);
         // event.target.innerHTML = '<img src = "ticTacImg/player-x.svg"></img>';
         // event.target.innerHTML = '<img src = "ticTacImg/player-o.svg"></img>'
 
@@ -24,12 +26,38 @@ window.addEventListener('DOMContentLoaded', () => {
             target.innerHTML = '<img src = "ticTacImg/player-o.svg"></img>'
             counter++
             checkArray[squareNum] = 'o'
-        } else {
-            return;
-        }
+        }; 
+        // console.log(checkArray)
         console.log(checkArray)
+        
+        let announce = document.getElementById('game-status');
+        for (let i1 = 0; i1 < checkArray.length; i1 += 3) {
+            if (checkArray[i1] === '') {
+                return;
+            }
+    
+    
+            if (checkArray[i1] === checkArray[i1 + 1] && checkArray[i1 + 1] === checkArray[i1 + 2]) {
+    
+                announce.innerText = `Winner: ${checkArray[i1].toUpperCase()}`;
+            }
+    
+        
+        };
+    
+        // Second Loop breaks the code. 
+        for (let i2 = 0; i2 < 3; i2++) {
+            if (checkArray[i2] === checkArray[i2 + 3] && checkArray[i2 + 3] === checkArray[i2 + 6]) {
+    
+                // let announce = document.getElementById('game-status');
+                announce.innerText = `Winner: ${checkArray[i2].toUpperCase()}`;
+                console.log(i2)
+            }
+        }
+        
+        
     });
-
-
+    
+    
 
 })
