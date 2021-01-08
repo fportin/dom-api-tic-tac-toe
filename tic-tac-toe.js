6b74ac6242144bafc66b72475fb2c6338bbd72b9
Kyle <k.powers005@gmail.com>
2021-01-08 16:43:26 -0500

    all checks completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,8 +3,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('tic-tac-toe-board');
     const squares = board.children;
     let counter = 0;
-    let checkArray = ['', '', '', 
-                       '', '', '', 
+    let checkArray = ['', '', '',
+                       '', '', '',
                        '', '', ''];
 
     board.addEventListener('click', event => {
@@ -26,38 +26,45 @@ window.addEventListener('DOMContentLoaded', () => {
             target.innerHTML = '<img src = "ticTacImg/player-o.svg"></img>'
             counter++
             checkArray[squareNum] = 'o'
-        }; 
-        // console.log(checkArray)
-        console.log(checkArray)
-        
-        let announce = document.getElementById('game-status');
-        for (let i1 = 0; i1 < checkArray.length; i1 += 3) {
-            if (checkArray[i1] === '') {
-                return;
-            }
-    
-    
-            if (checkArray[i1] === checkArray[i1 + 1] && checkArray[i1 + 1] === checkArray[i1 + 2]) {
-    
-                announce.innerText = `Winner: ${checkArray[i1].toUpperCase()}`;
-            }
-    
-        
         };
-    
-        // Second Loop breaks the code. 
-        for (let i2 = 0; i2 < 3; i2++) {
-            if (checkArray[i2] === checkArray[i2 + 3] && checkArray[i2 + 3] === checkArray[i2 + 6]) {
-    
-                // let announce = document.getElementById('game-status');
-                announce.innerText = `Winner: ${checkArray[i2].toUpperCase()}`;
-                console.log(i2)
+        // console.log(checkArray)
+        // console.log(checkArray)
+
+        let announce = document.getElementById('game-status');
+        for (let i = 0; i < 9; i += 3) {
+            if (checkArray[i] !== '' &&
+            checkArray[i] === checkArray[i + 1] &&
+            checkArray[i] === checkArray[i + 2]) {
+                announce.innerText = `Winner: ${checkArray[i].toUpperCase()}`;
             }
         }
-        
-        
+
+        for (let j = 0; j < 3; j++){
+            if (checkArray[j] !== '' &&
+            checkArray[j] === checkArray[j + 3] &&
+            checkArray[j] === checkArray[j + 6]) {
+                announce.innerText = `Winner: ${checkArray[j].toUpperCase()}`;
+            }
+
+        }
+
+        if (checkArray[0] !== '' &&
+        checkArray[0] === checkArray[4] &&
+        checkArray[0] === checkArray[8]) {
+            announce.innerText = `Winner: ${checkArray[0].toUpperCase()}`;
+        }
+
+        if (checkArray[2] !== '' &&
+        checkArray[2] === checkArray[4] &&
+        checkArray[2] === checkArray[6]) {
+            announce.innerText = `Winner: ${checkArray[2].toUpperCase()}`;
+        }
+
+        if (!checkArray.includes('')) {
+            announce.innerText = `Winner: NONE`
+        }
     });
-    
-    
+
+
 
 })
